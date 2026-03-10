e77a3d7752e672550cb667868799708b15a16274
Filipe Augusto Silva Maia <filipeasmaia@gmail.com>
2026-03-10 20:07:13 -0300

    Create Conexao.java

---
+++ Conexao.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
